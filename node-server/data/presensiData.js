3025facf6a288e966defa5f8f3b2d67dcea0ffc8
SyandyArda <syandi.2901@gmail.com>
2025-10-18 19:04:47 +0700

    Tugas 3

---
+++ node-server/data/presensiData.js
@@ -0,0 +1,10 @@
+const presensiRecords = [
+// Kita bisa mulai dengan data contoh
+{
+userId: 456,
+nama: 'Syandy Arda',
+checkIn: new Date('2025-10-14T08:05:00'),
+checkOut: new Date('2025-10-14T17:00:00')
+}
+];
+module.exports = presensiRecords;
--- node-server/server.js
+++ node-server/server.js
@@ -1,36 +1,28 @@
-// server.js
-
-const express = require('express');
-const cors = require('cors');
-const bookRoutes = require('./routes/books');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
+const morgan = require("morgan");
+
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
 // Middleware
-app.use(cors()); // Mengaktifkan CORS
-app.use(express.json()); // Body parser untuk JSON
-
-// Middleware untuk logging setiap request
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
-
-// Menggunakan routing dari books.js untuk endpoint /api/books
-app.use('/api/books', bookRoutes);
-
-// Middleware untuk menangani 404 Not Found
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Resource not found' });
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
 });
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong on the server' });
-});
-
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
