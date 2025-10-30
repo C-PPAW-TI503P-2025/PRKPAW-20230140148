71b45a54b7de373946372c4a9cc018447908e7d5
Arsyan <syandi.2901@gmail.com>
2025-10-29 22:19:54 -0700

    code akhir prak 5

--- node-server/routes/reports.js
+++ node-server/routes/reports.js
@@ -4,3 +4,29 @@ const router = express.Router();
  	const { addUserData, isAdmin } = require('../middleware/permissionMiddleware');
  	router.get('/daily', [addUserData, isAdmin], reportController.getDailyReport);
  	module.exports = router;
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama } = req.query;
+    let options = { where: {} };
+
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: records,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil laporan", error: error.message });
+  }
+};
